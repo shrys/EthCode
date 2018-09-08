eb7f90e06c533a8e081a5125e50c8e2979c4484d
shrys <shreyas770@yahoo.in>
2018-09-09 00:00:17 +0530

    contract deployment to ganache network

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,18 +1,31 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // uppercase, instatiate with ctor
+const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 let accounts;
+let inbox;
 
 beforeEach(async () => {
-    accounts = await web3.eth.getAccounts(); // get test accounts, async
+    // get test accounts, async
+    accounts = await web3.eth.getAccounts();
     
+    // use an account for deployment
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hi there!']
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
 });
 
 describe('Inbox', () => {
     it('deploys contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 });
 
